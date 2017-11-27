f8656f413c8b8afc4f7ffb1e1ebd743f975d91b3
christhopher.leao <christhopherleao@icloud.com>
2017-11-27 09:59:50 -0200

    fix in join

--- template-join.php
+++ template-join.php
@@ -4,13 +4,13 @@ get_header();
 
 <main role="main" aria-label="Content">
 
-<section class="contain" style="margin-top: 6rem">
+<!-- <section class="contain" style="margin-top: 6rem">
     <h2>Join our Excellence Program</h2>
     <img src="<?php echo get_bloginfo('template_url') ?>/build/images/process.png" />
-</section>
+</section> -->
 
-<section class="contain" style="margin-top: 4rem">
-    <h2>Apply Now</h2>
+<section class="contain layout-column-center" style="margin-top: 4rem">
+    <h2 style="margin: 0 0 4rem -120px; font-weight: bold">APPLY NOW</h2>
     <!-- sandbox="allow-scripts allow-popups allow-same-origin allow-top-navigation allow-pointer-lock allow-forms" -->
     <iframe src="//crm.planetexpat.org/apply" height="650" frameborder="0" class="job-board">
         <p>Your Browser is not cool. It Doesn't support iFrames</p>
